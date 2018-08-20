0385d070ee06a05d3fd4cb6570277945ec131cb7
Theo Askov <christianelbek@gmail.com>
2018-08-20 17:08:27 +0200

    searchmap component working with new ol version

--- src/app/searchmap/searchmap.component.ts
+++ src/app/searchmap/searchmap.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import Map from 'ol/Map';
-import Vector from 'ol/layer/Vector';
+
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import {Draw, Modify} from 'ol/interaction.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
 import WKT from 'ol/format/WKT';
-import Draw from 'ol/interaction/Draw';
-import Modify from 'ol/interaction/Modify';
-import Tile from 'ol/layer/Tile';
-import View from 'ol/View';
+import DrawEvent from 'ol/events';
 import Feature from 'ol/Feature';
-import GeometryType from 'ol/geom/GeometryType';
-import * as epsg3857 from 'ol/proj/epsg3857';
-import * as olsource from 'ol/source';
-import * as olstyle from 'ol/style';
+import * as epsg3857 from 'ol/proj/epsg3857.js';
+import {Style, Fill, Stroke, Circle as CircleStyle} from 'ol/style.js';
+import * as control from 'ol/control';
 
 import { environment } from '../../environments/environment';
 import { OccurenceService} from '../occurence.service';
+
+var draw, modify;
 
 @Component({
   selector: 'app-searchmap',
@@ -24,60 +26,62 @@ export class SearchmapComponent implements OnInit{
   @Output() setWkt: EventEmitter<string> = new EventEmitter();
   private map : Map;
   private layers : any;
-  private vector : Vector;
-  private source : olsource.Vector;
+  private vectorLayer : VectorLayer;
+  private vectorSource : VectorSource;
   private modify : Modify;
   private draw : Draw;
-  private type : GeometryType = GeometryType.Polygon;
+  private selectType : String;
   private wktFormat : WKT;
 
   constructor(private occurenceService:OccurenceService) { 
     this.occurenceService = occurenceService;
     this.wktFormat = new WKT();
-    this.source = new olsource.Vector();
-    this.vector = new Vector({
-      source: this.source,
-      style: new olstyle.Style({
-        fill: new olstyle.Fill({
+    this.vectorSource = new VectorSource();
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource,
+      style: new Style({
+        fill: new Fill({
           color: 'rgba(255, 255, 255, 0.2)'
         }),
-        stroke: new olstyle.Stroke({
+        stroke: new Stroke({
           color: '#ffcc33',
           width: 2
         }),
-        image: new olstyle.Circle({
+        image: new CircleStyle({
           radius: 7,
-          fill: new olstyle.Fill({
+          fill: new Fill({
             color: '#ffcc33'
           })
         })
       })
     });
+    this.selectType = "Polygon";
   
-    this.layers = [new Tile({
-      source: new olsource.OSM(),
+    this.layers = [new TileLayer({
+      source: new OSM(),
       visible: true
     }),
-    this.vector];      
+    this.vectorLayer];      
   } //constructor
 
   addInteractions() {
-    this.modify = new Modify({source: this.source});
+    this.modify = new Modify({source: this.vectorSource});
     this.draw = new Draw({
-      type: this.type
+      source: this.vectorSource,
+      type: this.selectType
     });
     this.map.addInteraction(this.draw);
     this.map.addInteraction(this.modify); 
-
-    this.draw.on('drawend', function(evt : any){  
+    var that = this;
+    this.draw.on('drawend', function(evt : any){
+      evt.preventDefault();
       var geom = evt.feature.getGeometry().transform('EPSG:3857','EPSG:4326');
       var feature = new Feature({
-        geometry: geom.getGeometry()
-      });
-      var wkt = this.wktFormat.writeFeature(feature);
-      this.setWkt.emit(wkt);
-      this.source.addFeature(evt.feature);
-    },this);    
+         geometry: geom
+       });
+      var wkt = that.wktFormat.writeFeature(feature);
+      that.setWkt.emit(wkt);
+    },that);    
   }
 
   ngOnInit() {
@@ -85,8 +89,8 @@ export class SearchmapComponent implements OnInit{
       target: 'mapdiv',
       layers: this.layers,
       // controls: control.defaults().extend([
-      //     new control.ScaleLine()
-      // ]),
+      //    new control.ScaleLine()
+      //  ]),
       view: new View({
           projection: environment.MAP_PROJECTION,
           center: epsg3857.fromEPSG4326(environment.MAP_CENTER_LONLAT),
