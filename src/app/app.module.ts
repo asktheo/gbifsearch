7de65c7da8c19d223f2c6824171ce6a32bacd843
Theo Askov <christianelbek@gmail.com>
2018-08-20 23:31:39 +0200

    update to Angular Cli 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule} from '@angular/http';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AlertModule } from 'ngx-bootstrap';
 import { FormsModule } from '@angular/forms';
@@ -28,4 +28,5 @@ import { OccurenceResultComponent } from './occurence-result/occurence-result.co
   providers: [HttpClientModule, OccurenceService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
